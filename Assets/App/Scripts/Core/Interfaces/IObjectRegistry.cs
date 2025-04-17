0132e86bf32cd830064e922d518abaf0b310ad37
Sergei Chikov <s.chikov@colynn.pro>
2025-04-18 02:26:02 +0700

    Simple Registry for managing Objects

---
+++ Assets/App/Scripts/Core/Interfaces/IObjectRegistry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace App.Scripts.Core
+{
+    // GenericInterface for storing objects 
+    public interface IObjectRegistry<T>
+    {
+        public IEnumerable<T> GetRegistry();
+        public void AddObject(T regObject);
+        public void RemoveObject(T regObject);
+        public bool TryGetRandomObject(out T regObject);
+    }
+}
---
+++ Assets/App/Scripts/Core/ObjectRegistry.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Scripts.Core
+{
+    public class ObjectRegistry<T> : IObjectRegistry<T>
+    {
+        protected List<T> _registry = new();
+
+        public IEnumerable<T> GetRegistry()
+        {
+            return _registry;
+        }
+
+        public virtual void AddObject(T regObject)
+        {
+            _registry.Add(regObject);
+        }
+
+        public virtual void RemoveObject(T regObject)
+        {
+            _registry.Remove(regObject);
+        }
+
+        public virtual bool TryGetRandomObject(out T regObject)
+        {
+            regObject = default;
+
+            if (_registry.Count > 0)
+            {
+                regObject = _registry[Random.Range(0, _registry.Count)];
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
